007c26f175bdb366922a7c661d8f539114954c7b
Evgeny <ea.nikulin@yandex.ru>
2021-07-16 10:13:38 +0500

    // upd buttons

--- web_statement/main/views.py
+++ web_statement/main/views.py
@@ -3,7 +3,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страниw',
+        'values': ['some', 'hello'],
+        'obj': {
+            'car1': 'bmw1',
+            'car2': 'bmw2',
+            'car3': 'bmw3',
+        }
+
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
+
+def test(request):
+    return render(request, 'main/test.html')
