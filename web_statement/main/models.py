36af137a9e5c6c54ddbfcf61446b1d25adee4c07
Evgeny <ea.nikulin@yandex.ru>
2021-08-04 18:01:57 +0500

    //upd 0408 add fix

--- web_statement/main/models.py
+++ web_statement/main/models.py
@@ -13,18 +13,21 @@ from django.db import models
 #         return super(User, self).save(*args, **kwargs)
 
 class Bid(models.Model):
+
     created_date_bid = models.DateTimeField('Дата создания заявки', auto_now_add=True)
     modified_date_bid = models.DateTimeField('Дата изменения заявки', auto_now=True)
     num_bid = models.CharField('Номер заявки', max_length=250, default='ОР-')
-    source_bid = models.GenericIPAddressField('Источник')
-    recipient_bid = models.GenericIPAddressField('Получатель')
-    port_bid = models.IntegerField('Порт', default='0')
-    protocol_bid = models.CharField('Протокол', max_length=300, default='TCP')
-    description_bid = models.TextField('Описание')
-    time_rule = models.CharField('Тип правила: постоянное/временное', max_length=300, default='Постоянное')
-    date_rule_start = models.DateField('Дата начала действия')
-    date_rule_end = models.DateField('Дата окончания действия')
+    source_bid = models.GenericIPAddressField('Источник', default='192.168.0.1')
+    recipient_bid = models.GenericIPAddressField('Получатель', default='192.168.0.2')
+    port_bid = models.IntegerField('Порт', default='3389')
+    # protocol_bid = models.CharField('Протокол', max_length=300, default='TCP')
+    # protocol_bid = models.ForeignKey(protocol_bid),
+    protocol_bid = models.ForeignKey('Protocol_bid_class', on_delete=models.CASCADE,)
+    persistent_rule = models.BooleanField('Постоянное правило', default=True)
+    date_rule_start = models.DateField('Дата начала действия', null=True, blank=True)
+    date_rule_end = models.DateField('Дата окончания действия', null=True, blank=True)
     justification_bid = models.TextField('Обоснование')
+    description_bid = models.TextField('Описание')
 
 
     def __str__(self):
@@ -34,6 +37,10 @@ class Bid(models.Model):
         verbose_name = 'Заявка'
         verbose_name_plural = 'Заявки'
 
+class Protocol_bid_class(models.Model):
+    title = models.CharField('Тип протокола', max_length=300)
 
+    def __str__(self):
+        return self.title
 
 # Create your models here.
